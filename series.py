b76ea5f25b0d5461a7fd79f55814ce6bf833aa8e
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2020-04-07 21:40:35 +0530

    Update series.py

--- series.py
+++ series.py
@@ -1,4 +1,15 @@
 
+data  -- data takes various forms like ndarray, series, map, lists, dict, constants and also another DataFrame.
+
+index  -- For the row labels, the Index to be used for the resulting frame is Optional Default np.arange(n) if no index is passed.
+
+columns -- For column labels, the optional default syntax is - np.arange(n). This is only true if no index is passed.
+
+dtype -- Data type of each column.
+	
+copy -- This command (or whatever it is) is used for copying of data, if the default is False.
+
+
 #import the pandas library and aliasing as pd
 # import pandas as pd
 # import numpy as np
