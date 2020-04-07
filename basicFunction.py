5aa162e6b3e8287443995d1bee9aff651a0169ae
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2020-04-08 01:02:34 +0530

    Create basicFunction.py

---
+++ basicFunction.py
@@ -0,0 +1,10 @@
+axes == Returns a list of the row axis labels
+dtype == Returns the dtype of the object.
+empty == Returns True if series is empty.
+ndim == Returns the number of dimensions of the underlying data, by definition 1.
+size == Returns the number of elements in the underlying data.
+values == Returns the Series as ndarray.
+head() == Returns the first n rows.
+tail() == Returns the last n rows.
+
+
