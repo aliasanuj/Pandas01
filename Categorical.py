7b9d23928a3de1ae1a04f2fd2eaa44ff52319cd6
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2020-04-11 00:29:13 +0530

    Create Categorical.py

---
+++ Categorical.py
@@ -0,0 +1,19 @@
+# import pandas as pd
+# s = pd.Series(["a","b","c","a"], dtype="category")
+# print(s)
+# 0    a
+# 1    b
+# 2    c
+# 3    a
+# dtype: category
+# Categories (3, object): [a, b, c]
+
+
+
+
+
+# import pandas as pd
+# cat = pd.Categorical(['a', 'b', 'c', 'a', 'b', 'c'])
+# print(cat)
+# [a, b, c, a, b, c]
+# Categories (3, object): [a, b, c]
