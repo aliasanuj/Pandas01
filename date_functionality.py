341d4b8e4b4d7a5d85c630d50462645e4434684b
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2020-04-10 21:09:56 +0530

    Update and rename data_functionality.py to date_functionality.py

---
+++ date_functionality.py
@@ -0,0 +1,64 @@
+data_functionality.py
+
+
+
+#Create a Range of Dates
+
+
+
+# import pandas as pd
+# print(pd.date_range('1/1/2011', periods=5))
+# DatetimeIndex(['2011-01-01', '2011-01-02', '2011-01-03', '2011-01-04',
+#                '2011-01-05'],
+#               dtype='datetime64[ns]', freq='D')
+
+
+
+
+# import pandas as pd
+# print(pd.date_range('1/1/2011', periods=5,freq='M'))
+# DatetimeIndex(['2011-01-31', '2011-02-28', '2011-03-31', '2011-04-30',
+#                '2011-05-31'],
+#               dtype='datetime64[ns]', freq='M')
+#
+
+
+
+# import pandas as pd
+# print(pd.date_range('1/1/2011', periods=5))
+# DatetimeIndex(['2011-01-01', '2011-01-02', '2011-01-03', '2011-01-04',
+#                '2011-01-05'],
+#               dtype='datetime64[ns]', freq='D')
+
+
+
+# import pandas as pd
+# start = pd.datetime(2011, 1, 1)
+# end = pd.datetime(2011, 1, 5)
+# print(pd.date_range(start, end))
+# DatetimeIndex(['2011-01-01', '2011-01-02', '2011-01-03', '2011-01-04',
+#                '2011-01-05'],
+#               dtype='datetime64[ns]', freq='D')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
