a1b821375ad5629c49ed801ebdddaea76c8fc661
unknown <aliasanuj>
2020-04-11 19:52:44 +0530

    change in series

--- series.py
+++ series.py
@@ -88,6 +88,20 @@
 #import the pandas library and aliasing as pd
 # import pandas as pd
 # import numpy as np
+# data = {'a' : 0, 'b' : 1, 'c' : 2,'x':np.nan}
+# s = pd.Series(data)
+# print(s)
+# a    0.0
+# b    1.0
+# c    2.0
+# x    NaN
+# dtype: float64
+
+
+############################################################
+#import the pandas library and aliasing as pd
+# import pandas as pd
+# import numpy as np
 # data = {'a' : 0., 'b' : 1., 'c' : 2.}
 # s = pd.Series(data,index=['b','c','d','a'])
 # print (s)
@@ -126,6 +140,23 @@
 # 3    5
 # dtype: int64
 
+
+
+##################################
+#import the pandas library and aliasing as pd
+# import pandas as pd
+# import numpy as np
+# s = pd.Series(5, index=[0, 1, 2, 3,'a'])
+# print(s)
+# 0    5
+# 1    5
+# 2    5
+# 3    5
+# a    5
+# dtype: int64
+
+
+
 ##################################
 #import the pandas library and aliasing as pd
 # import pandas as pd
@@ -137,22 +168,8 @@
 # dtype: int64
 #
 
-##################################
-
-# import pandas as pd
-# s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
-# #retrieve the first element
-# print(s)
-# a    1
-# b    2
-# c    3
-# d    4
-# e    5
-# dtype: int64
 
 ##################################
-
-
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve the first element
@@ -166,16 +183,16 @@
 
 
 
-
-
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve the first element
 # print(s[2])
 # 3
 
-##################################
 
+
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve the first three element
@@ -185,8 +202,9 @@
 # c    3
 # dtype: int64
 
-##################################
 
+
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve the first three element
@@ -198,8 +216,8 @@
 # e    5
 # dtype: int64
 
-##################################
 
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve the first three element
@@ -208,8 +226,9 @@
 # e    5
 # dtype: int64
 
-##################################
 
+
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve the last three element
@@ -219,17 +238,17 @@
 # e    5
 # dtype: int64
 
+
+
 ##################################
-
-
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve a single element
 # print(s['c'])
 # 3
 
-##################################
 
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve multiple elements
@@ -237,8 +256,8 @@
 # a    1
 # dtype: int64
 
-##################################
 
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve multiple elements
@@ -248,8 +267,9 @@
 # d    4
 # dtype: int64
 
-##################################
 
+
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve multiple elements
@@ -258,8 +278,10 @@
 # except KeyError:
 #     print("not present")
 
-##################################
 
+
+
+##################################
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve multiple elements
@@ -269,16 +291,38 @@
 #     print("not present")
 #not present
 
+
+
+
 ##############################33
-
 # import pandas as pd
-# s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
+# import numpy as np
+# s = pd.Series([1,2,3,4,np.nan],index = ['a','b','c','d','e'])
 # #retrieve the first element
-# print (s[2])
-# 3
-#####################################3
+# print (s)
+# a    1.0
+# b    2.0
+# c    3.0
+# d    4.0
+# e    NaN
+# dtype: float64
 
 
+
+
+
+#####################################
+# import pandas as pd
+# import numpy as np
+# s = pd.Series([1,2,3,4,np.nan],index = ['a','b','c','d','e'])
+# #retrieve the first element
+# print (s['e'])
+# nan
+
+
+
+
+########################################3
 # import pandas as pd
 # s = pd.Series([1,2,3,4,5],index = ['a','b','c','d','e'])
 # #retrieve the first element
