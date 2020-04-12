e9b21ae63248f9f2209a5bdcf994f92249d81d78
unknown <aliasanuj>
2020-04-12 15:52:18 +0530

    dcndjkcnjds

--- basicFunction.py
+++ basicFunction.py
@@ -1,13 +1,15 @@
-series:
-axes == Returns a list of the row axis labels
-dtype == Returns the dtype of the object.
-empty == Returns True if series is empty.
-ndim == Returns the number of dimensions of the underlying data, by definition 1.
-size == Returns the number of elements in the underlying data.
-values == Returns the Series as ndarray.
-head() == Returns the first n rows.
-tail() == Returns the last n rows.
-============================================================
+#series:
+# axes == Returns a list of the row axis labels
+# dtype == Returns the dtype of the object.
+# empty == Returns True if series is empty.
+# ndim == Returns the number of dimensions of the underlying data, by definition 1.
+# size == Returns the number of elements in the underlying data.
+# values == Returns the Series as ndarray.
+# head() == Returns the first n rows.
+# tail() == Returns the last n rows.
+
+
+########################################################
 # import pandas as pd
 # import numpy as np
 # #Create a series with 100 random numbers
@@ -23,6 +25,7 @@ tail() == Returns the last n rows.
 
 
 
+########################################################
 # import pandas as pd
 # import numpy as np
 # #Create a series with 100 random numbers
