fe9654bd130823d29ebd723293ac6581aa2d74b1
unknown <aliasanuj>
2020-04-14 01:30:08 +0530

    tome_delta

--- time_Delta.py
+++ time_Delta.py
@@ -1,32 +1,32 @@
-time_Delta.py
-
-
-
-
+#################################################################
 # import pandas as pd
 # print(pd.Timedelta('2 days 2 hours 15 minutes 30 seconds'))
 # #2 days 02:15:30
 
 
+
+#################################################################
 # import pandas as pd
 # print(pd.Timedelta(6,unit='h'))
 # # 0 days 06:00:00
 
 
+
+#################################################################
 # import pandas as pd
 # print(pd.Timedelta(days=2))
 # 2 days 00:00:00
 
 
 
-
+#################################################################
 # import pandas as pd
 # print(pd.Timedelta(days=2))
 # 2 days 00:00:00
 #
 
 
-
+#################################################################
 # import pandas as pd
 # s = pd.Series(pd.date_range('2012-1-1', periods=3, freq='D'))
 # td = pd.Series([ pd.Timedelta(days=i) for i in range(3) ])
@@ -41,7 +41,7 @@ time_Delta.py
 
 
 
-
+#################################################################
 # import pandas as pd
 # s = pd.Series(pd.date_range('2012-1-1', periods=3, freq='D'))
 # td = pd.Series([ pd.Timedelta(days=i) for i in range(3) ])
@@ -57,7 +57,7 @@ time_Delta.py
 
 
 
-
+#################################################################
 # import pandas as pd
 # s = pd.Series(pd.date_range('2012-1-1', periods=3, freq='D'))
 # td = pd.Series([ pd.Timedelta(days=i) for i in range(3) ])
@@ -69,24 +69,6 @@ time_Delta.py
 # 0 2012-01-01 0 days 2012-01-01 2012-01-01
 # 1 2012-01-02 1 days 2012-01-03 2012-01-04
 # 2 2012-01-03 2 days 2012-01-05 2012-01-07
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
