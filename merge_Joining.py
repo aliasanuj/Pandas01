7fdb71d740fe2aae5e7048b78a2e77379254d4a9
Kumar Anuj <40429093+aliasanuj@users.noreply.github.com>
2020-04-17 13:55:06 +0530

    Update merge_Joining.py

--- merge_Joining.py
+++ merge_Joining.py
@@ -14,13 +14,17 @@
 # left − A DataFrame object.
 # right − Another DataFrame object.
 # on − Columns (names) to join on. Must be found in both the left and right DataFrame objects.
-# left_on − Columns from the left DataFrame to use as keys. Can either be column names or arrays with length equal to the length of the DataFrame.
-# right_on − Columns from the right DataFrame to use as keys. Can either be column names or arrays with length equal to the length of the DataFrame.
-# left_index − If True, use the index (row labels) from the left DataFrame as its join key(s). In case of a DataFrame with a MultiIndex (hierarchical),
+# left_on − Columns from the left DataFrame to use as keys. Can either be column names or arrays with length equal to the 
+#length of the DataFrame.
+# right_on − Columns from the right DataFrame to use as keys. Can either be column names or arrays with length equal to the 
+#length of the DataFrame.
+# left_index − If True, use the index (row labels) from the left DataFrame as its join key(s). In case of a DataFrame with a 
+#MultiIndex (hierarchical),
 # the number of levels must match the number of join keys from the right DataFrame.
 # right_index − Same usage as left_index for the right DataFrame.
 # how − One of 'left', 'right', 'outer', 'inner'. Defaults to inner. Each method has been described below.
-# sort − Sort the result DataFrame by the join keys in lexicographical order. Defaults to True, setting to False will improve the performance substantially in many cases.
+# sort − Sort the result DataFrame by the join keys in lexicographical order. Defaults to True, setting to False will 
+#improve the performance substantially in many cases.
 
 
 
